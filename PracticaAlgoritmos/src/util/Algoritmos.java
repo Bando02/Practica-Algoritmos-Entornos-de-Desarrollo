1f8fb7d817ef9fda97a88e13f41171342946cdcc
fvp02 <ztfernando002@gmail.com>
2024-04-18 12:37:03 +0200

    Metodo factorial

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -4,7 +4,12 @@ public abstract class Algoritmos {
 	
 	
 	
-	
+	/**
+	 * Metodo utilizado para obtener el numero fibonacci 
+	 * 
+	 * @param numero
+	 * @return
+	 */
 	public static int fibonacci(int numero) {
 		if(numero == 0) {
 			return 0;
@@ -14,5 +19,21 @@ public abstract class Algoritmos {
 			return fibonacci(numero-1) + fibonacci(numero-2);
 		}	
 	}
+	
+	public static int factorial(int numero) {
+		
+		if(numero == 0 || numero == 1) {
+			return 1;
+		}else {
+			int factorial = 1;
+			for(int i = 2 ; i <= numero ; i++) {
+				factorial = factorial*1;
+			}
+			return factorial;
+		}
+		
+	}
+	
+	
 
 }
