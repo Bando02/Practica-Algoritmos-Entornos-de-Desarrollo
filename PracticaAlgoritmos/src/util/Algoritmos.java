589de426bcc978d595db09373c8efde5b4744e26
fvp02 <ztfernando002@gmail.com>
2024-04-18 13:39:57 +0200

    Añado metodo para saber si es primo

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -34,6 +34,16 @@ public abstract class Algoritmos {
 		
 	}
 	
+	public static boolean esPrimo(int numero) {
+		if (numero <= 1){
+			return false;
+		}for(int i=2;i<numero;i++) {
+	        if(numero%i==0)
+	            return false;
+	    }
+	    return true;	
+	}
+	
 	
 
 }
