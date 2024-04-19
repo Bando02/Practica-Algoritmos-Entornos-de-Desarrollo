2275d1cf07f46de9dd0b27f2c737efd8512411fc
fvp02 <ztfernando002@gmail.com>
2024-04-19 09:48:32 +0200

    Depuracion correcta

--- PracticaAlgoritmos/src/app/Principal.java
+++ PracticaAlgoritmos/src/app/Principal.java
@@ -1,59 +1,48 @@
 package app;
+
 import java.util.Arrays;
 import util.Algoritmos;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
-	int[]numeros = new int[5];
-	int[]resultadoFibonacci = new int[5];
-	int[]resultadoFactorial = new int[5];
-	boolean[]resultadoPrimo = new boolean[5];
-	int a = 0;
-	int b = 0;
-	int fibo = 0;
-	int facto = 0;
-	
-	
-	for(int i = 0 ; i < 5 ; i++) {
-	
-		a = (int)Math.random();
-		
-		numeros[i] = a;		
-	}
-	for(int i = 0 ; i<5 ; i++) {
-		
-		
-		b = numeros[i];
-		
-		fibo = Algoritmos.fibonacci(b);
-		
-		resultadoFibonacci[i] = fibo;
-		
-	}
-	for(int i = 0 ; i<5 ; i++) {
-		
-		
-		b = numeros[i];
-		
-		facto = Algoritmos.factorial(b);
-		
-		resultadoFactorial[i] = facto;
-		
-	}
-	for(int i = 0 ; i<5 ; i++) {
-				
-		b = numeros[i];
-		
-		resultadoPrimo[i] = Algoritmos.esPrimo(b);
-		
-		 
-		
-	}
 
+		int[] numeros = new int[5];
+		int[] resultadoFibonacci = new int[5];
+		int[] resultadoFactorial = new int[5];
+		boolean[] resultadoPrimo = new boolean[5];
+
+		for (int i = 0; i < numeros.length; i++) {
+			int a = (int) (Math.random()*10);
+			numeros[i] = a;
+		}
 		
-		
+		for (int i = 0; i < 5; i++) {
+
+			int b = numeros[i];
+
+			int fibo = Algoritmos.fibonacci(b);
+
+			resultadoFibonacci[i] = fibo;
+
+		}
+		for (int i = 0; i < numeros.length; i++) {
+
+			int b = numeros[i];
+
+			int facto = Algoritmos.factorial(b);
+
+			resultadoFactorial[i] = facto;
+
+		}
+		for (int i = 0; i < numeros.length; i++) {
+
+			int b = numeros[i];
+
+			resultadoPrimo[i] = Algoritmos.esPrimo(b);
+
+		}
+
 	}
 
 }
